8806a642b2255fda80fce3ac162fda8605cc46c1
Ksimolg <43907445+Ksimolg@users.noreply.github.com>
2023-06-07 14:09:45 +0300

    first commit

---
+++ empty-for-git/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  empty-for-git
+//
+//  Created by Роман Исламов on 07.06.2023.
+//
+
+import Foundation
---
+++ empty-for-git/modelView.swift
@@ -0,0 +1,9 @@
+//
+//  modelView.swift
+//  empty-for-git
+//
+//  Created by Роман Исламов on 07.06.2023.
+//
+
+import Foundation
+func add() {}
